e644b3827a37211da9e88a24ba54eb51e80f8f05
Bojan Markovic <bolom992@gmail.com>
2019-05-24 17:08:07 +0200

    Initial project setup. Added TabBarController with two view controllers(ListViewController, MapViewController). Set icons for list, map tabs and titles.

---
+++ DarwinDigitalTest/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  DarwinDigitalTest
+//
+//  Created by Bojan Markovic on 24/05/2019.
+//  Copyright © 2019 Bojan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class MapViewController: UIViewController {
+    
+}
